eb98839fef400ef574af0119c08b1b047ea1950d
Benjamin <benjamin@debian>
2017-04-30 22:30:09 +0200

    Suppression de fichiers useless

--- Carte.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Carte {
-	private int listeCarteChanceBonus[] = {};
-	private int listeCarteChanceMalus[];
-	private int listeCarteCommunauteBonus[];
-	private int listeCarteCommunauteMalus[];
-}	// Fin de la classe Carte
