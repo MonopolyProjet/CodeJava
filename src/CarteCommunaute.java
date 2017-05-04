c781ef7917c0a79c94076729853e54c65fe36ffc
shifty13626 <ludovic.hamel@u-psud.fr>
2017-05-04 18:40:23 +0200

    classe communaute avec le premier main

--- src/CarteCommunaute.java
+++ src/CarteCommunaute.java
@@ -6,16 +6,15 @@ import java.util.Scanner;
 public class CarteCommunaute extends Carte{
 	private int numCarteCommunaute;
 	private String texte;
-	private String type;
+	
 	
 	// on va crï¿½er un constructeur qui construit les cartes a partir de la lecture d'un fichier .txt
 	CarteCommunaute (int i) { // est l'indice de la carte a creer
 		int nbCarteChance = i;
 		String description = ""; // va recevoir le texte de la carte
-		String type =""; // va recevoir le type de la carte (bonus malus)
-		
+				
 		// on dï¿½clare le nouveau fichier
-		File f = new File ("carte"+i +".txt");
+		File f = new File ("src/CarteCaisseDeCommunaute/carte"+i +".txt");
 		
 		// si le fichier existe on va faire les opï¿½ration suivante
 		if (f.exists())
@@ -37,13 +36,35 @@ public class CarteCommunaute extends Carte{
 			Scanner sc = new Scanner (fis);
 			// on attrape maintenant ligne par ligne (tant qu'il y en a)
 				description = sc.nextLine();
-				type = sc.nextLine();
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 		this.texte = description;
-		this.type = type;
 	} // fin du constructeur
-}
+	
+	
+	// methode toString pour afficher une carte chance
+	public String toString () {
+		String s = "";
+		s = s +"num carte : " +numCarteCommunaute +"\n" +texte;
+		return s;
+	}
+	
+	/////////////////////////////////////////////////////
+	//////////////// FONCTION MAIN //////////////////////
+	/////////////////////////////////////////////////////
+	
+	public static void main (String [] args) {
+	// on va créer 3 cartes (les trois premieres)
+	CarteCommunaute c1 = new CarteCommunaute (1);
+	CarteCommunaute c2 = new CarteCommunaute (2);
+	CarteCommunaute c3 = new CarteCommunaute (3);
+	
+	// on va les affichers
+	System.out.println(c1);
+	System.out.println(c2);
+	System.out.println(c3);
+	}
+} // fin de la classe
