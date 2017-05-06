da1f9b8513b9c8f2d829f29eccbd0ba25cd257bd
shifty13626 <ludovic.hamel@u-psud.fr>
2017-05-06 11:00:30 +0200

    Classe joueur avec le main complet et fonctionnel + toute les methodes nécessaires

--- src/Joueur.java
+++ src/Joueur.java
@@ -49,8 +49,14 @@ public class Joueur{
 	
 	// methode pour acheter une propriete
 	public void acheterCase (Case c) {
-		this.listePropriete.add(c);
-		this.argent -= c.getPrix();
+		//on va verifier qu'il peut l'acheter
+		if (argent >= c.getPrix())
+		{
+			this.listePropriete.add(c);
+			this.argent -= c.getPrix();
+		}
+		else
+			System.out.println("Le joueur n'a pas assez d'argent");
 	}
 
 	public void payerTaxe(int montantTaxe){
@@ -80,16 +86,61 @@ public class Joueur{
 		return trouve;
 	}
 	
+	// methode pour afficher toutes les propriété d'un joueur sous forme de liste
+	private void afficherPropri() {
+		System.out.println("Le joueur possède toute les propriétés suivantes : " +"\n");
+		// on parcourt toutes ses propriétes et on les affiche
+		for (int i=0; i<listePropriete.size(); i++)
+		{
+			System.out.println(listePropriete.get(i) +"\n");
+		}
+	}
+	
 	// methode toString du joueur
 	public String toString() {
 		String s = "";
 		
-		s = s +"Le joueur s'appel : " +nom +" Il a la couleur " +couleur +" et a : " +argent +" milles";
+		s = s +"Le joueur s'appel : " +nom +", il a la couleur " +couleur +" et a : " +argent +" milles";
 		return s;
 	}
 	
-	public static void main(String[] args){
-		Joueur Bob = new Joueur("Bob","Jaune");
-	}
 	
+	//////////////////////////////////////////////////
+	/////////// FONCTION MAIN ////////////////////////
+	//////////////////////////////////////////////////
+	public static void main (String [] args) {
+		Joueur j = new Joueur("Bob","Jaune");
+		j.avancerJoueur(6);
+		//on affiche le nom du joueur
+		System.out.println("Le nom du joueur est : " +j.getNom());
+		// on affiche la case actuelle du joueur
+		System.out.println("La case actuelle du joueur est : " +"\n" +j.getCaseActuelle());
+		// on affiche l'argent qu'il a 
+		System.out.println("Le joueur a : " +j.getArgent() +" mille(s)");
+		// on lui ajoute 10 mille et on lui retirera mille
+		j.gagneArgent(10000);
+		System.out.println("Le joueur a : " +j.getArgent() +" mille(s) apres l'ajout");
+		j.retirerArgent(1000);
+		System.out.println("Le joueur a : " +j.getArgent() +" mille(s) apres le retrait");
+		
+		//on va lui faire acheter une propriete
+		Case c = new Case("champs");
+		
+		j.acheterCase(c);
+		// on va vérifier qu'il est la case
+		if (j.aCase (c))
+			System.out.println("Le joueur a bien la case : " +c);
+		else
+			System.out.println("La transaction n'a pas fonctionné");
+		
+		// on va afficher toute les propriétés du joueur
+		Case c2 = new Case("pigalle");
+		j.acheterCase(c2);
+		j.afficherPropri();
+		
+		// on affiche le joueur directement
+		System.out.println(j);
+		
+	} // fin main
+		
 } // Fin de la classe joueur
