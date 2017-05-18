3625c1cef60f00d73857c4e7235a413d0fea8b72
houin.baptist91@gmail.com <De3f784e72!>
2017-05-18 19:09:23 +0200

    Fond : tout mit dans le constructeur -> Image s'affiche plus

--- src/Fond.java
+++ src/Fond.java
@@ -8,49 +8,64 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Fond extends JFrame{
-	Image img = ImageIO.read(new File ("fond.jpg)"));
-	//ImageLoader Image=new ImageLoader();
+
+	private static final long serialVersionUID = 3827913327888251420L;
+	//Image img = ImageIO.read(new File ("fond.jpg)"));
+	private JFrame frame;// = new JFrame();
+	private JPanel pnlFond;// = new JPanel();
+	private JPanel pnlCmd;// = new JPanel();
+	private BorderLayout bL;// = new BorderLayout();
+	private JTextArea tA;// = new JTextArea();
+	
 	public Fond() throws FileNotFoundException, IOException {
 		this.setTitle("Monopoly");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setLocationRelativeTo(null);
-	    //this.setResizable(false); à voir
+	    this.setResizable(false); // à voir
+	    this.frame = new JFrame();
+	    this.pnlFond=new JPanel();
+	    this.pnlCmd=new JPanel();
+	    this.bL = new BorderLayout();
+	    this.tA = new JTextArea();
 	    
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Graphics g = null;
-		JFrame frame = new JFrame();
-		JPanel pnlFond = new JPanel();
-		JPanel pnlCmd = new JPanel();
-		GridLayout gL = new GridLayout(3,1);
-		gL.setHgap(50);
-		gL.setVgap(200);
+		Graphics g = null;		
+
+		bL.setHgap(50);
+		bL.setVgap(200);
 		
-		pnlCmd.setLayout(gL);
+		pnlCmd.setLayout(bL);
+		
+		//creation des boutons
 		JButton nvPartie = new JButton ("Nouvelle partie");
 		JButton chPartie = new JButton ("Charger partie");
+		JButton svPartie = new JButton ("Sauver partie");
 		JButton quit = new JButton ("Quitter");
-		nvPartie.setBounds(1225, 625, 100, 50);
-		chPartie.setBounds(1225, 525, 100, 50);
-		quit.setBounds(1225, 425, 100, 50);
 		
+		//Ajout des boutons au panel
 		pnlCmd.add(nvPartie);
 		pnlCmd.add(chPartie);
+		pnlCmd.add(svPartie);
 		pnlCmd.add(quit);
-
-		//pnlCmd.setBounds(1225, 525, 300, 200);
+		
+		// Reglage de la taille du panel et de la photo de fond de la frame
 		pnlCmd.setSize(400, 400);
 		pnlFond.add(new JLabel(new ImageIcon("src/Fond/fondMonop.jpg")));
 		pnlFond.setSize(1050, 1050);
+		pnlFond.setVisible(true);
 		
 		frame.setLayout(new BorderLayout());
 
 		frame.getContentPane().add(pnlFond,BorderLayout.WEST);
 		frame.getContentPane().add(pnlCmd, BorderLayout.CENTER);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1400,1050);
-		frame.setVisible(true);
+			}
+	
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		Fond f = new Fond();
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(1400,1050);
+		f.setVisible(true);
 		
 	}
 
