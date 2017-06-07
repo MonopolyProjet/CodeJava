b7cfc4cf3e2076a4050f54160260d35d0cc56107
bMazoin <mazoinbenjamin@gmail.com>
2017-06-07 18:51:36 +0200

    Modification Fond.java + ajout image plateau dans dossier src

--- src/Fond.java
+++ src/Fond.java
@@ -1,72 +1,94 @@
 package src;
 
-import java.awt.*;
-
-import java.io.*;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Fond extends JFrame{
 
-	private static final long serialVersionUID = 3827913327888251420L;
-	//Image img = ImageIO.read(new File ("fond.jpg)"));
-	private JFrame frame;// = new JFrame();
-	private JPanel pnlFond;// = new JPanel();
-	private JPanel pnlCmd;// = new JPanel();
-	private BorderLayout bL;// = new BorderLayout();
-	private JTextArea tA;// = new JTextArea();
+public class Fond extends JPanel implements ActionListener{
 	
-	public Fond() throws FileNotFoundException, IOException {
-		this.setTitle("Monopoly");
-	    this.setLocationRelativeTo(null);
-	    this.setResizable(false); // à voir
-	    this.frame = new JFrame();
-	    this.pnlFond=new JPanel();
-	    this.pnlCmd=new JPanel();
-	    this.bL = new BorderLayout();
-	    this.tA = new JTextArea();
-	    
-		Graphics g = null;		
-
-		bL.setHgap(50);
-		bL.setVgap(200);
-		
-		pnlCmd.setLayout(bL);
-		
-		//creation des boutons
-		JButton nvPartie = new JButton ("Nouvelle partie");
-		JButton chPartie = new JButton ("Charger partie");
-		JButton svPartie = new JButton ("Sauver partie");
-		JButton quit = new JButton ("Quitter");
-		
-		//Ajout des boutons au panel
-		pnlCmd.add(nvPartie);
-		pnlCmd.add(chPartie);
-		pnlCmd.add(svPartie);
-		pnlCmd.add(quit);
-		
-		// Reglage de la taille du panel et de la photo de fond de la frame
-		pnlCmd.setSize(400, 400);
-		pnlFond.add(new JLabel(new ImageIcon("src/Fond/fondMonop.jpg")));
-		pnlFond.setSize(1050, 1050);
-		pnlFond.setVisible(true);
-		
-		frame.setLayout(new BorderLayout());
-
-		frame.getContentPane().add(pnlFond,BorderLayout.WEST);
-		frame.getContentPane().add(pnlCmd, BorderLayout.CENTER);
-			}
+	private JButton btnNouvellePartie = new JButton("Nouvelle Partie");
+	private JButton btnSauverPartie = new JButton("Sauver Partie");
+	private JButton btnChargerPartie = new JButton("Charger Partie");
+	private JButton btnFinTour = new JButton("Fin Tour");
+	private JButton btnAcheter = new JButton("Acheter");
 	
+	private JPanel panelGauche = new JPanel();
+	private JLabel fondMonopoly = new JLabel("", SwingConstants.CENTER);
+	private Icon imageMonopoly = new ImageIcon("src/imageMonopoly.jpg");
 	
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-		// TODO Auto-generated method stub
-		Fond f = new Fond();
+	private JPanel panelDroit = new JPanel();
+	private JPanel panelSolde = new JPanel();
+	private JPanel panelInterraction = new JPanel();
+	private JPanel panelBoutons = new JPanel();
+	private JTextField zone1 = new JTextField();
+	private JLabel soldePlateau = new JLabel("Solde Plateau : ");
+	private JLabel soldeJoueur = new JLabel("Solde Joueur : ");
 		
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(1400,1050);
-		f.setVisible(true);
+	Fond (){
+		
+		Font f = new Font("Yu Gothic UI Semibold", Font.PLAIN, 36);
+		JFrame mainFrame = new JFrame();
+		GridLayout layoutGauche = new GridLayout(1,1);
+		BorderLayout layoutDroit = new BorderLayout(); //Utile aussi pour le panel Solde
+		
+		
+		//Panel Gauche
+		panelGauche.setLayout(layoutGauche);
+		panelGauche.setBackground(Color.BLACK);
+		fondMonopoly.setIcon(imageMonopoly);
+		panelGauche.add(fondMonopoly);
+		mainFrame.add(panelGauche);
+		
+		//Panel Boutons qui contient tous les boutons
+		panelBoutons.setLayout(new GridLayout(4,2));
+		panelBoutons.add(btnNouvellePartie);
+		panelBoutons.add(btnSauverPartie);
+		panelBoutons.add(btnChargerPartie);
+		panelBoutons.add(btnFinTour);
+		panelBoutons.add(btnAcheter);
+		
+		//Panel communication va contenir les retours du jeu + le panel pour les boutons
+		panelInterraction.setLayout(new GridLayout(2,1));
+		panelInterraction.add(zone1);
+		zone1.setEnabled(false);
+		panelInterraction.add(panelBoutons);
+
+		//Panel Droit
+		panelDroit.setLayout(layoutDroit);
+		panelSolde.setLayout(new GridLayout(1,2));
+		soldePlateau.setFont(f);
+		soldeJoueur.setFont(f);
+		panelSolde.add(soldePlateau);
+		panelSolde.add(soldeJoueur);	
+		panelDroit.add(BorderLayout.NORTH, panelSolde);
+		panelDroit.add(BorderLayout.CENTER, panelInterraction);
+		mainFrame.add(panelDroit);
+		
+		mainFrame.setLayout(new GridLayout(1,2));
+		mainFrame.pack();
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setVisible(true);
+		mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		btnNouvellePartie.addActionListener(this);
+		btnSauverPartie.addActionListener(this);
+		btnChargerPartie.addActionListener(this);
+		btnFinTour.addActionListener(this);
+		btnAcheter.addActionListener(this);
+	} 
+	
+	public void actionPerformed(ActionEvent e){	
 		
 	}
-
+	
+	public static void main (String args[]){
+		Fond f = new Fond();
+		f.setSize(1400,1050);
+		f.setVisible(true);	
+		
+		
+	}
 }
